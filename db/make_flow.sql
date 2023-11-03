ad27fc1ed734e97adc087f35da918ced428e9e81
feng_wf <feng_wf@outlook.com>
2023-11-03 10:11:38 +0800

    feature: 相关文档

---
+++ db/make_flow.sql
@@ -0,0 +1,136 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 本地mysql8
+ Source Server Type    : MySQL
+ Source Server Version : 80032
+ Source Host           : localhost:3306
+ Source Schema         : make_flow
+
+ Target Server Type    : MySQL
+ Target Server Version : 80032
+ File Encoding         : 65001
+
+ Date: 18/07/2023 09:31:46
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for makeflow_activity
+-- ----------------------------
+DROP TABLE IF EXISTS `makeflow_activity`;
+CREATE TABLE `makeflow_activity`  (
+  `id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '主键',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
+  `creator` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '创建人',
+  `updator` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '更新人',
+  `del_flag` tinyint NOT NULL COMMENT '是否删除',
+  `name` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '模板名字',
+  `activity_type` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '活动类型',
+  `start_time` datetime NULL DEFAULT NULL COMMENT '开始时间',
+  `end_time` datetime NULL DEFAULT NULL COMMENT '结束时间',
+  `status` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '状态',
+  `flow_inst_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '流程实列id',
+  `definition_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '流程定义id',
+  `parent_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '父活动',
+  `pre_activity_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '前一个节点id',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_flow_inst_id`(`flow_inst_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for makeflow_execute
+-- ----------------------------
+DROP TABLE IF EXISTS `makeflow_execute`;
+CREATE TABLE `makeflow_execute`  (
+  `id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '主键',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
+  `creator` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '创建人',
+  `updator` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '更新人',
+  `del_flag` tinyint NOT NULL COMMENT '是否删除',
+  `root_flow_inst_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '根流程实列id',
+  `activity_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '当前活动id',
+  `activity_code_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '当前活动codeId',
+  `start_time` datetime NULL DEFAULT NULL COMMENT '开始时间',
+  `end_time` datetime NULL DEFAULT NULL COMMENT '结束时间',
+  `status` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '状态',
+  `flow_inst_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '流程实列id',
+  `definition_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '流程定义id',
+  `parent_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '父活动',
+  `variables` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '动态参数',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_flow_inst_id`(`flow_inst_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for makeflow_flow_inst
+-- ----------------------------
+DROP TABLE IF EXISTS `makeflow_flow_inst`;
+CREATE TABLE `makeflow_flow_inst`  (
+  `id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '主键',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
+  `creator` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '创建人',
+  `updator` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '更新人',
+  `del_flag` tinyint NOT NULL COMMENT '是否删除',
+  `defintion_code_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '根流程实列id',
+  `start_time` datetime NULL DEFAULT NULL COMMENT '开始时间',
+  `end_time` datetime NULL DEFAULT NULL COMMENT '结束时间',
+  `apply_time` datetime NULL DEFAULT NULL COMMENT '结束时间',
+  `status` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '状态',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_defintion_code_id`(`defintion_code_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for makeflow_task
+-- ----------------------------
+DROP TABLE IF EXISTS `makeflow_task`;
+CREATE TABLE `makeflow_task`  (
+  `id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '主键',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
+  `creator` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '创建人',
+  `updator` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '更新人',
+  `del_flag` tinyint NOT NULL COMMENT '是否删除',
+  `name` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '任务名',
+  `hanlder` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '处理人',
+  `flow_inst_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '流程id',
+  `activity_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '节点id',
+  `activity_code_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '节点codeId',
+  `status` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '状态',
+  `complte_time` datetime NULL DEFAULT NULL COMMENT '完成时间',
+  `task_type` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '任务类型',
+  `opinion` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '意见',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_flow_inst_id`(`flow_inst_id`) USING BTREE,
+  INDEX `idx_activity_id`(`activity_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for makeflow_template
+-- ----------------------------
+DROP TABLE IF EXISTS `makeflow_template`;
+CREATE TABLE `makeflow_template`  (
+  `id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '主键',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
+  `creator` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '创建人',
+  `updator` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '更新人',
+  `del_flag` tinyint NOT NULL COMMENT '是否删除',
+  `name` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '模板名字',
+  `flow_process_code_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '模板的codeId',
+  `state` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '状态（1启用',
+  `max` tinyint NOT NULL COMMENT '是否最新版本（1，最新，0，不是最新）',
+  `version` int NOT NULL COMMENT '版本',
+  `data_type` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '数据类型',
+  `template_data` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '模板数据',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_flow_process_code_id`(`flow_process_code_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
---
+++ file/makeflow操作手册.md
@@ -0,0 +1,174 @@
+- 引入流程引擎依赖
+
+```xml
+        <dependency>
+            <groupId>com.makeid.makeflow</groupId>
+            <artifactId>makeflow-service-Application</artifactId>
+            <version>1.0</version>
+        </dependency>
+
+
+```
+
+- 引入流程引擎需要的驱动（mysql,mongodb二选一）
+
+
+
+```xml
+        <!-- MySQL驱动 -->
+        <dependency>
+            <groupId>mysql</groupId>
+            <artifactId>mysql-connector-java</artifactId>
+            <version>${mysql.version}</version>
+            <scope>provided</scope>
+        </dependency>
+     <!--   <dependency>
+            <groupId>org.mongodb</groupId>
+            <artifactId>mongo-java-driver</artifactId>
+            <version>${mongo.version}</version>
+            <scope>provided</scope>
+        </dependency>
+-->
+```
+
+- 在对应启动类或者配置类上加入@ImportMakeFlow注解 启动流程引擎容器
+
+```java
+@ImportMakeFlow
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class,args);
+    }
+}
+```
+
+- 根据实际情况 在yaml中 完善流程引擎的配置参数如数据库数据源，redis相关配置   
+
+```yml
+# MongoDB
+makeflow:
+  data:
+    dataType: mysql
+    mongo:
+      replicaSet: 127.0.0.1:27017
+      dbname: makeflow
+      connectionsPerHost: 200
+      threadsAllowedToBlockForConnectionMultiplier: 10
+      maxWaitTime: 4000
+      connectTimeout: 4000
+      socketTimeout: 5000
+      autoConnectRetry: true
+      readPreference: primaryPreferred
+      username: root
+      password: root
+      authdb: admin
+    mysql:
+      druid-data-source:
+        username: root
+        password: root
+        url: jdbc:mysql://127.0.0.1:3306/make_flow?useUnicode=true&characterEncoding=UTF-8&serverTimezone=Asia/Shanghai&rewriteBatchedStatements=true&useSSL=false&allowPublicKeyRetrieval=true
+        driver-class-name: com.mysql.cj.jdbc.Driver
+        type: com.alibaba.druid.pool.DruidDataSource
+        initialSize: 5
+        minIdle: 5
+        maxActive: 20
+        maxWait: 60000
+        timeBetweenEvictionRunsMillis: 60000
+        minEvictableIdleTimeMillis: 300000
+        validationQuery: SELECT 1 FROM DUAL
+        testWhileIdle: true
+        testOnBorrow: false
+        testOnReturn: false
+        poolPreparedStatements: true
+        maxPoolPreparedStatementPerConnectionSize: 20
+  redis:
+    server:
+      # 单机：stand-alone
+      host: 127.0.0.1
+      password: 123456
+      port: 6379
+      database: 0
+      type: stand-alone
+      #     哨兵：sentinel ；主从： 或者 master-slave  集群：cluster
+      #        master: mymaster
+      #        nodes: 172.16.39.18:26376,172.16.39.18:26377,172.16.39.18:26378
+      #        password: csp172@eb5g
+      #        database: 0
+      #        type: sentinel
+      #     集群：cluster
+      #    nodes: 172.16.39.18:26376,172.16.39.18:26377,172.16.39.18:26378
+      #    password: csp172@eb5g
+      #    database: 0
+      #    type: cluster
+      #获取锁最长等待时间,单位秒
+      waitTime: 10
+      #锁过期最长时间，单位秒
+      leaseTime: 10
+```
+
+- 通过门面MakeFlowApplication获取对应管理流程的对象
+
+```java
+Long s = MakeFlowApplication.getWorkFlowService().submitFlow(submitVO.getCodeId(), submitVO.getFlowInstId(), submitVO.getUserId(), submitVO.getVariables());
+```
+
+- 可自主实现FlowEventListener 并监听自己想关心的事件
+
+```java
+package com.makeid.demo.listener;
+
+import com.makeid.makeflow.workflow.delegate.DelegateExecuteReader;
+import com.makeid.makeflow.workflow.delegate.DelegateTaskReader;
+import com.makeid.makeflow.workflow.event.FlowEventListener;
+import com.makeid.makeflow.workflow.event.ProcessStartedEvent;
+import com.makeid.makeflow.workflow.event.TaskDoneEvent;
+import com.makeid.makeflow.workflow.event.TaskRunningEvent;
+import com.makeid.makeflow.workflow.eventbus.FlowSubscribe;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * @author feng_wf
+ * @program makeflow-demo
+ * @description
+ * @create 2023-10-09
+ */
+@Slf4j
+@Component
+public class TaskListener implements FlowEventListener {
+
+    @FlowSubscribe(sync = false)
+    public void business1(TaskRunningEvent taskRunningEvent) {
+        List<? extends DelegateTaskReader> data = taskRunningEvent.getData();
+        data.stream().forEach(delegateTaskReader -> {
+            log.info("监听器收到  任务已经运行【{}】",delegateTaskReader.getName());
+        });
+    }
+
+    @FlowSubscribe(sync = false)
+    public void business1(TaskDoneEvent taskDoneEvent) {
+        List<? extends DelegateTaskReader> data = taskDoneEvent.getData();
+        data.stream().forEach(delegateTaskReader -> {
+            log.info("监听器收到 任务已经完成【{}】 处理人【{}】",delegateTaskReader.getName(),delegateTaskReader.getHandler());
+        });
+    }
+}
+
+```
+
+- 实现源码
+
+```url
+https://gitlab.xsoul.cn:9500/wangfeng/makeflow-service
+```
+
+- 实现demo
+
+```url
+https://gitlab.xsoul.cn:9500/wangfeng/makeflow-demo
+```
+
+
